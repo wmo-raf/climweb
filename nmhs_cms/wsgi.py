f1ddbe907ebe5b6471ea885d03d4bc55b2c13f2c
Grace Amondi <miswa.grace@gmail.com>
2023-03-29 11:25:47 +0300

    add megamenu for services

--- nmhs_cms/wsgi.py
+++ nmhs_cms/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", f"nmhs_cms.settings.{os.getenv('ENVIRONMENT', 'dev')}")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", f"nmhs_cms.settings.{os.getenv('ENVIRONMENT', 'production')}")
 
 application = get_wsgi_application()
