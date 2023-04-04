c04b6abdb596111506f6b6ee9476e56f2b295204
Grace Amondi <miswa.grace@gmail.com>
2023-04-05 01:21:21 +0300

    add 404 and 500 page

--- nmhs_cms/urls.py
+++ nmhs_cms/urls.py
@@ -55,6 +55,7 @@ urlpatterns = [
 if settings.DEBUG:
     from django.conf.urls.static import static
     from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+    from django.views.generic import TemplateView
 
     # Serve static and media files from development server
     urlpatterns += staticfiles_urlpatterns()
@@ -63,8 +64,12 @@ if settings.DEBUG:
     import debug_toolbar
 
     urlpatterns = [
-                      path(f"{os.getenv('BASE_PATH', '')}"+'__debug__/', include(debug_toolbar.urls)),
+                    path(f"{os.getenv('BASE_PATH', '')}"+'__debug__/', include(debug_toolbar.urls)),
+                    path(f"{os.getenv('BASE_PATH', '')}"+"test404/", TemplateView.as_view(template_name="404.html")),
+                    path(f"{os.getenv('BASE_PATH', '')}"+"test500/", TemplateView.as_view(template_name="500.html")),
                   ] + urlpatterns
+    
+  
 
 urlpatterns = urlpatterns + i18n_patterns(
     # For anything not caught by a more specific rule above, hand over to
