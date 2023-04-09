266fa999e13bf5310ab54d9a0f94c62a7f4a5999
Grace Amondi <miswa.grace@gmail.com>
2023-04-09 15:18:57 +0300

    add theme

--- site_settings/context_processors.py
+++ site_settings/context_processors.py
@@ -4,12 +4,16 @@ from site_settings.models import Theme
 def theme(request):
 
     theme_ls = Theme.objects.get(is_default = True)
-    print("CURRENT THEME:", theme_ls)
 
-    theme = {
-        'primary_color': theme_ls.primary_color,
-        'secondary_color': theme_ls.secondary_color,
-        'border_radius': f"{theme_ls.border_radius * 0.06}em",
-    }
+    if theme_ls:
+        theme = {
+            'primary_color': theme_ls.primary_color,
+            'secondary_color': theme_ls.secondary_color,
+            'border_radius': f"{theme_ls.border_radius * 0.06}em",
+        }
+    else:
+        theme = {
+            
+        }
 
     return theme
