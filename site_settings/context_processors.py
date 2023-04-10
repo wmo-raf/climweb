58d631539ab04b3e82f6e6a24be34141bdf440b2
Grace Amondi <miswa.grace@gmail.com>
2023-04-10 12:20:39 +0300

    add box shadow

--- site_settings/context_processors.py
+++ site_settings/context_processors.py
@@ -10,10 +10,11 @@ def theme(request):
             'primary_color': theme_ls.primary_color,
             'secondary_color': theme_ls.secondary_color,
             'border_radius': f"{theme_ls.border_radius * 0.06}em",
+            'box_shadow': f"elevation-{theme_ls.box_shadow}",
         }
     else:
         theme = {
-            
+
         }
 
     return theme
