c1ffd9c29f8c9d6726813fbec5b1a01c4bbeb0c3
Grace Amondi <miswa.grace@gmail.com>
2023-06-15 10:32:33 +0300

    clean up migrations

--- site_settings/management/commands/load_countries.py
+++ site_settings/management/commands/load_countries.py
@@ -1,29 +1,15 @@
-import logging
 import json
 
 from django.core.management.base import BaseCommand
 from django.contrib.gis.geos import GEOSGeometry
 from site_settings.models import Country
 
-
-# Define the base URL for the Met Norway API
-BASE_URL = "https://api.met.no/weatherapi/locationforecast/2.0/compact"
-headers = {
-  'User-Agent':'Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/111.0.0.0 Mobile Safari/537.36'
-}
-
-logger = logging.getLogger(__name__)
-
-default_options = () if not hasattr(BaseCommand, 'option_list') \
-    else BaseCommand.option_list
-
-
 class Command(BaseCommand):
     help = ('autotranslate all the message files that have been generated '
             'using the `makemessages` command.')
 
     def handle(self, *args, **options):
-        with open('/home/app/web/nmhs-cms/site_settings/data/countries_extents.geojson') as f:
+        with open('/home/app/web/nmhs-cms/countries_extents.geojson') as f:
             data = json.load(f)
 
         for feature in data['features']:
