4fa455196cbbf36fd4ae409b5cba56aa0ec93e80
Grace Amondi <miswa.grace@gmail.com>
2023-08-04 20:13:41 +0300

    add amharic and arabic

--- docs/conf.py
+++ docs/conf.py
@@ -175,6 +175,7 @@ intersphinx_mapping = {
 # of the sidebar.
 # html_logo = 'logo.png'
 
+
 # The name of an image file (within the static path) to use as favicon of the
 # docs.  This file should be a Windows icon file (.ico) being 16x16 or 32x32
 # pixels large.
