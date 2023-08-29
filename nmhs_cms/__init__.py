787b21a1d4aca971749a4210a2d723891ca4ac2e
Grace Amondi <miswa.grace@gmail.com>
2023-08-29 12:45:43 +0300

    dynamic cms version

--- nmhs_cms/__init__.py
+++ nmhs_cms/__init__.py
@@ -1,7 +1,9 @@
 from nmhs_cms.utils.version import get_version
+from nmhs_cms.settings.base import CMS_VERSION
 
 # major.minor.patch.release.number
 # release must be one of alpha, beta, rc, or final
-VERSION = (0, 5, 3, "final", 0)
+print(CMS_VERSION.split('.'))
+VERSION = (CMS_VERSION.split('.')[0], CMS_VERSION.split('.')[1], CMS_VERSION.split('.')[2], "final", 0)
 
 __version__ = get_version(VERSION)
