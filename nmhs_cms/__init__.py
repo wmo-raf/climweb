0c80bc9fd0efabad5c929151f45f020715414409
Grace Amondi <miswa.grace@gmail.com>
2023-08-30 15:16:00 +0300

    dynamic doc release version

--- nmhs_cms/__init__.py
+++ nmhs_cms/__init__.py
@@ -1,8 +1,11 @@
 from nmhs_cms.utils.version import get_version
-from nmhs_cms.settings.base import CMS_VERSION
+# from nmhs_cms.settings.base import CMS_VERSION
 
 # major.minor.patch.release.number
 # release must be one of alpha, beta, rc, or final
-VERSION = (CMS_VERSION.split('.')[0], CMS_VERSION.split('.')[1], CMS_VERSION.split('.')[2], "final", 0)
+# VERSION = (CMS_VERSION.split('.')[0], CMS_VERSION.split('.')[1], CMS_VERSION.split('.')[2], "final", 0)
+
+# TODO: Dynamic versioning 
+VERSION = (0, 6, 9, "final" , 0) 
 
 __version__ = get_version(VERSION)
