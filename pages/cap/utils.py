271370901354840c21b8114cfb0fdb1e734526ba
Erick Otenyo <otenyo.erick@gmail.com>
2023-09-29 13:05:05 +0300

    Updates

--- pages/cap/utils.py
+++ pages/cap/utils.py
@@ -1,3 +1,4 @@
+import os
 from io import BytesIO
 
 import cartopy.feature as cf
@@ -5,6 +6,9 @@ import geopandas as gpd
 import matplotlib
 import matplotlib.pyplot as plt
 from cartopy import crs as ccrs
+from django.conf import settings
+from matplotlib.offsetbox import OffsetImage, AnnotationBbox
+import matplotlib.image as mpimg
 
 matplotlib.use('Agg')
 
@@ -16,14 +20,15 @@ def cap_geojson_to_image(geojson_feature_collection, options, extents=None):
     width = 3.6
 
     fig = plt.figure(figsize=(width, height))
-    ax = plt.axes([0, 0, 1, 1], projection=ccrs.PlateCarree())
+    # ax = plt.axes([0, 0, 1, 1], projection=ccrs.PlateCarree())
+    ax = plt.subplot(1, 1, 1, projection=ccrs.PlateCarree())
 
     # set line width
     [x.set_linewidth(0.2) for x in ax.spines.values()]
 
     # set extent
-    if extents:
-        ax.set_extent(extents, crs=ccrs.PlateCarree())
+    # if extents:
+    #     ax.set_extent(extents, crs=ccrs.PlateCarree())
 
     # add country borders
     ax.add_feature(cf.LAND)
@@ -36,9 +41,24 @@ def cap_geojson_to_image(geojson_feature_collection, options, extents=None):
     gdf.apply(lambda x: ax.annotate(text=x["areaDesc"], xy=x.geometry.centroid.coords[0], ha='center', fontsize=7),
               axis=1)
 
-    # add title
+    # add logo
+    if options.get("org_logo"):
+        logo_file = options.get("org_logo").file.path
+        logo_path = os.path.join(settings.MEDIA_ROOT, logo_file)
+        logo_img = mpimg.imread(logo_path)
+        logo = OffsetImage(logo_img, zoom=0.1)
+        logo_position = (0.5, 0.5)
+        logo_box = AnnotationBbox(logo,
+                                  logo_position,
+                                  xycoords='axes fraction',
+                                  box_alignment=(0, 0),
+                                  pad=0,
+                                  frameon=False)
+        ax.add_artist(logo_box)
+
+        # add title
     if options.get("title"):
-        plt.title(options.get("title"), fontsize=9)
+        plt.title(options.get("title"), fontsize=8)
 
     # create plot
     buffer = BytesIO()
