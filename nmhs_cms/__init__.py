6cadffeeeea559d9fd51f3450c6d3d970b925e65
Grace Amondi <miswa.grace@gmail.com>
2024-08-02 16:59:17 +0300

    beta to final

--- nmhs_cms/__init__.py
+++ nmhs_cms/__init__.py
@@ -2,7 +2,7 @@ from nmhs_cms.utils.version import get_version, get_semver_version
 
 # major.minor.patch.release.number
 # release must be one of alpha, beta, rc, or final
-VERSION = (0, 9, 1, "beta", 1)
+VERSION = (0, 9, 1, "final", 1)
 
 __version__ = get_version(VERSION)
 
