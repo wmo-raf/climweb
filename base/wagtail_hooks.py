6a70b66f5b76dab14f2607078dbc5bd08d6405b9
Erick Otenyo <otenyo.erick@gmail.com>
2024-08-06 11:55:21 +0300

    Add aviation menu view permission

--- base/wagtail_hooks.py
+++ base/wagtail_hooks.py
@@ -162,7 +162,8 @@ def register_permissions():
 def hide_menu_items(request, menu_items):
     custom_menu_permissions = {
         "geo-manager": "base.can_view_geomanager_menu",
-        "city_forecast": "base.can_view_forecast_menu"
+        "city_forecast": "base.can_view_forecast_menu",
+        "aviation_editor": "base.can_view_aviation_editor_menu",
     }
 
     hidden = []
