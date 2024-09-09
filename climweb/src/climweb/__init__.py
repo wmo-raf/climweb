fd892dc2f4002f6fa435f38780aefe8f26c3cfb2
Erick Otenyo <otenyo.erick@gmail.com>
2024-09-09 13:11:47 +0300

    Update version

--- climweb/src/climweb/__init__.py
+++ climweb/src/climweb/__init__.py
@@ -2,7 +2,7 @@ from .utils.version import get_version, get_semver_version
 
 # major.minor.patch.release.number
 # release must be one of alpha, beta, rc, or final
-VERSION = (0, 9, 3, "final", 0)
+VERSION = (0, 9, 4, "beta", 1)
 
 __version__ = get_version(VERSION)
 
