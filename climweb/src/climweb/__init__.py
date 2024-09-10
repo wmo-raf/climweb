19bcecbb4630241b795066b481fc32d0552feb91
Erick Otenyo <otenyo.erick@gmail.com>
2024-09-10 09:07:28 +0300

    Update getting version in setup.py

--- climweb/src/climweb/__init__.py
+++ climweb/src/climweb/__init__.py
@@ -1,10 +1,9 @@
-from .config.celery_init import app as celery_app
-from .utils.version import get_version, get_semver_version
-
-# major.minor.patch.release.number
-# release must be one of alpha, beta, rc, or final
-VERSION = (0, 9, 4, "beta", 1)
+from climweb.config.celery_init import app as celery_app
+from climweb.utils.version import get_version, get_semver_version
+from climweb.version import VERSION
 
 __version__ = get_version(VERSION)
 
 __semver__ = get_semver_version(VERSION)
+
+__all__ = ["celery_app"]
---
+++ climweb/src/climweb/version.py
@@ -0,0 +1,3 @@
+# major.minor.patch.release.number
+# release must be one of alpha, beta, rc, or final
+VERSION = (0, 9, 4, "beta", 1)
