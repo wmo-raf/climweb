a4e169ac1e7e6d4dfd46ba5fdaa5f7a48d9545fc
Erick Otenyo <otenyo.erick@gmail.com>
2024-09-18 06:59:46 +0300

    Add wdqms_stats tasks to use celery, rather than system crontab

--- climweb/src/climweb/base/tasks.py
+++ climweb/src/climweb/base/tasks.py
@@ -35,6 +35,15 @@ def clear_old_forecasts(self):
     call_command('clear_old_forecasts')
 
 
+@app.task(base=Singleton, bind=True)
+def run_wdqms_stats(self, variable):
+    # Log that the task is starting
+    self.logger.info(f"Running wdqms_stats for {variable}")
+
+    # Run the `wdqms_stats` management command
+    call_command('wdqms_stats', '-var', variable)
+
+
 @app.on_after_finalize.connect
 def setup_periodic_tasks(sender, **kwargs):
     # run_backup every day at midnight
@@ -56,4 +65,39 @@ def setup_periodic_tasks(sender, **kwargs):
         crontab(hour=0, minute=0),
         clear_old_forecasts.s(),
         name="clear-old-forecasts-every-day-midnight",
+    )
+
+    # Schedule task for pressure at 00:00 and 12:00
+    sender.add_periodic_task(
+        crontab(hour='0,12', minute=0),
+        run_wdqms_stats.s('pressure'),
+        name='Run wdqms_stats for pressure at 00:00 and 12:00'
+    )
+
+    # Schedule task for temperature at 00:00 and 12:00
+    sender.add_periodic_task(
+        crontab(hour='0,12', minute=0),
+        run_wdqms_stats.s('temperature'),
+        name='Run wdqms_stats for temperature at 00:00 and 12:00'
+    )
+
+    # Schedule task for humidity at 00:00 and 12:00
+    sender.add_periodic_task(
+        crontab(hour='0,12', minute=0),
+        run_wdqms_stats.s('humidity'),
+        name='Run wdqms_stats for humidity at 00:00 and 12:00'
+    )
+
+    # Schedule task for meridional_wind at 00:00 and 12:00
+    sender.add_periodic_task(
+        crontab(hour='0,12', minute=0),
+        run_wdqms_stats.s('meridional_wind'),
+        name='Run wdqms_stats for meridional_wind at 00:00 and 12:00'
+    )
+
+    # Schedule task for zonal_wind at 00:00 and 12:00
+    sender.add_periodic_task(
+        crontab(hour='0,12', minute=0),
+        run_wdqms_stats.s('zonal_wind'),
+        name='Run wdqms_stats for zonal_wind at 00:00 and 12:00'
     )
