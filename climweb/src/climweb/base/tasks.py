d0a4cb5cc4b2291a9fe4643715ffb550cc18d389
Erick Otenyo <otenyo.erick@gmail.com>
2024-09-18 06:52:20 +0300

    Add city forecast tasks to run with celery rather than system crontab

--- climweb/src/climweb/base/tasks.py
+++ climweb/src/climweb/base/tasks.py
@@ -17,10 +17,43 @@ def run_backup(self):
     call_command('mediabackup', '--clean', '--noinput')
 
 
+@app.task(
+    base=Singleton,
+    bind=True
+)
+def download_forecast(self):
+    # Run the `generate_forecast` command
+    call_command('generate_forecast')
+
+
+@app.task(
+    base=Singleton,
+    bind=True
+)
+def clear_old_forecasts(self):
+    # Run the `clear_old_forecasts` command
+    call_command('clear_old_forecasts')
+
+
 @app.on_after_finalize.connect
 def setup_periodic_tasks(sender, **kwargs):
+    # run_backup every day at midnight
     sender.add_periodic_task(
         crontab(hour=0, minute=0),
         run_backup.s(),
         name="run-backup-every-day-midnight",
+    )
+
+    # download_forecast every hour
+    sender.add_periodic_task(
+        crontab(minute=0),
+        download_forecast.s(),
+        name="download-forecast-every-hour",
+    )
+
+    # clear_old_forecasts every day at midnight
+    sender.add_periodic_task(
+        crontab(hour=0, minute=0),
+        clear_old_forecasts.s(),
+        name="clear-old-forecasts-every-day-midnight",
     )
