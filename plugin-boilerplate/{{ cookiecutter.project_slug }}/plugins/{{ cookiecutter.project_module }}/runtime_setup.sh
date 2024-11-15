c46443f13a7c5656d3351a0e7a1263f779b42991
Erick Otenyo <otenyo.erick@gmail.com>
2024-11-15 16:06:32 +0300

    Fix typo

--- plugin-boilerplate/{{ cookiecutter.project_slug }}/plugins/{{ cookiecutter.project_module }}/runtime_setup.sh
+++ plugin-boilerplate/{{ cookiecutter.project_slug }}/plugins/{{ cookiecutter.project_module }}/runtime_setup.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # Bash strict mode: http://redsymbol.net/articles/unofficial-bash-strict-mode/
-set -euo pipefailb
+set -euo pipefail
 # This file is automatically run by climweb when a climweb container starts up for the
 # first time with this plugin already installed and built. It should impotently run
 # any runtime setup that involves doing things with the  postgres database
