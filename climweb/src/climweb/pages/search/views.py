745f2748afa30e45f0e2f076d3151d534b4ecc2d
Erick Otenyo <otenyo.erick@gmail.com>
2025-01-23 14:02:51 +0300

    Update where wagtailsearch Query is imported from for new wagtail versions

--- climweb/src/climweb/pages/search/views.py
+++ climweb/src/climweb/pages/search/views.py
@@ -1,24 +1,23 @@
 from django.core.paginator import EmptyPage, PageNotAnInteger, Paginator
 from django.template.response import TemplateResponse
-
+from wagtail.contrib.search_promotions.models import Query
 from wagtail.models import Page
-from wagtail.search.models import Query
 
 
 def search(request):
     search_query = request.GET.get("query", None)
     page = request.GET.get("page", 1)
-
+    
     # Search
     if search_query:
         search_results = Page.objects.live().search(search_query)
         query = Query.get(search_query)
-
+        
         # Record hit
         query.add_hit()
     else:
         search_results = Page.objects.none()
-
+    
     # Pagination
     paginator = Paginator(search_results, 10)
     try:
@@ -27,7 +26,7 @@ def search(request):
         search_results = paginator.page(1)
     except EmptyPage:
         search_results = paginator.page(paginator.num_pages)
-
+    
     return TemplateResponse(
         request,
         "search/search.html",
