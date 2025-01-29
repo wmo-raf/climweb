54b066955b8862b02b1d12b663e1e7f7b25d58f6
Erick Otenyo <otenyo.erick@gmail.com>
2025-01-29 11:27:29 +0300

    Change introduction_title to CharField

--- climweb/src/climweb/pages/data_request/models.py
+++ climweb/src/climweb/pages/data_request/models.py
@@ -27,15 +27,15 @@ class DataRequestPage(MetadataPageMixin, WagtailCaptchaEmailForm):
     required_css_class = 'required'
     form_builder = CustomFormBuilder
     submissions_list_view_class = CustomSubmissionsListView
-
+    
     template = 'datarequest.html'
     parent_page_types = ['home.HomePage']
     subpage_types = []
     max_count = 1
     show_in_menus_default = True
     landing_page_template = 'form_thank_you_landing.html'
-
-    introduction_title = models.TextField()
+    
+    introduction_title = models.CharField(max_length=255, verbose_name=_("Introduction Title"))
     introduction_subtitle = models.TextField(blank=True, null=True)
     illustration_image = models.ForeignKey(
         'wagtailimages.Image',
@@ -47,7 +47,7 @@ class DataRequestPage(MetadataPageMixin, WagtailCaptchaEmailForm):
         related_name='+',
     )
     thank_you_text = models.TextField(blank=True, null=True)
-
+    
     content_panels = AbstractEmailForm.content_panels + [
         FieldPanel("introduction_title"),
         FieldPanel("introduction_subtitle"),
@@ -62,25 +62,25 @@ class DataRequestPage(MetadataPageMixin, WagtailCaptchaEmailForm):
             FieldPanel('subject'),
         ], "Email"),
     ]
-
+    
     def get_form_fields(self):
         return self.datarequest_form_fields.all()
-
+    
     def get_form_class(self):
         form_class = super(DataRequestPage, self).get_form_class()
         form_class.required_css_class = 'required'
         return form_class
-
+    
     def save(self, *args, **kwargs):
         if not self.search_description and self.introduction_subtitle:
             # Limit the search meta desc to google's 160 recommended chars
             self.search_description = truncatechars(self.introduction_subtitle, 160)
         return super().save(*args, **kwargs)
-
+    
     def serve(self, request, *args, **kwargs):
         if request.method == 'POST':
             form = self.get_form(request.POST, request.FILES, page=self, user=request.user)
-
+            
             if form.is_valid():
                 form_submission = None
                 try:
@@ -88,16 +88,16 @@ class DataRequestPage(MetadataPageMixin, WagtailCaptchaEmailForm):
                     duplicate_fields = get_duplicates(form.cleaned_data)
                 except Exception:
                     duplicate_fields = []
-
+                
                 if not duplicate_fields:
                     form_submission = self.process_form_submission(form)
                 else:
                     self.process_suspicious_form(form)
-
+                
                 return self.render_landing_page(request, form_submission, *args, **kwargs)
         else:
             form = self.get_form(page=self, user=request.user)
-
+        
         context = self.get_context(request)
         context['form'] = form
         return TemplateResponse(
@@ -105,14 +105,14 @@ class DataRequestPage(MetadataPageMixin, WagtailCaptchaEmailForm):
             self.get_template(request),
             context
         )
-
+    
     def process_suspicious_form(self, form):
         remove_captcha_field(form)
         try:
             self.send_suspicious_form_to_admin(form)
         except Exception:
             pass
-
+    
     def send_suspicious_form_to_admin(self, form):
         content = []
         for field in form:
@@ -122,7 +122,7 @@ class DataRequestPage(MetadataPageMixin, WagtailCaptchaEmailForm):
             content.append('{}: {}'.format(field.label, value))
         content = '\n'.join(content)
         mail_admins("POSSIBLE SPAM (DATA REQUEST PAGE)- {}".format(self.subject), content, fail_silently=True)
-
+    
     @staticmethod
     def get_image_title(filename):
         """
@@ -134,32 +134,32 @@ class DataRequestPage(MetadataPageMixin, WagtailCaptchaEmailForm):
             result = result.replace('-', ' ').replace('_', ' ')
             return result.title()
         return ''
-
+    
     def process_form_submission(self, form):
         cleaned_data = form.cleaned_data
-
+        
         for name, field in form.fields.items():
             file_type = None
             if isinstance(field, FormImageField):
                 file_type = 'image'
             elif isinstance(field, FormDocumentField):
                 file_type = 'document'
-
+            
             if file_type:
                 file = cleaned_data.get(name)
                 if file:
                     file.title = self.get_image_title(file.name)
                     file_type = file_type
-
+                    
                     file_submission = FormFileSubmission.objects.create(
                         file=file,
                         file_type=file_type,
                     )
-
+                    
                     cleaned_data[name] = file_submission.pk
                 else:
                     del cleaned_data[name]
-
+        
         return super(DataRequestPage, self).process_form_submission(form)
 
 
@@ -168,9 +168,9 @@ class DataRequestFormField(AbstractFormField):
         ("image", _("Upload Image")),
         ("document", _("Upload PDF Document")),
     )
-
+    
     field_type = models.CharField(
         verbose_name=_("field type"), max_length=16, choices=FORM_FIELD_CHOICES + FILE_SUBMISSION_FIELD_CHOICES
     )
-
+    
     page = ParentalKey(DataRequestPage, on_delete=models.CASCADE, related_name="datarequest_form_fields")
