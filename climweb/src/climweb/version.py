f4b8fd688375745edd728c3b00229f9dd64a1249
Erick Otenyo <otenyo.erick@gmail.com>
2025-03-11 10:57:16 +0300

    Bump version

--- climweb/src/climweb/version.py
+++ climweb/src/climweb/version.py
@@ -1,12 +1,12 @@
 # major.minor.patch.release.number
 # release must be one of alpha, beta, rc, or final
-VERSION = (0, 9, 5, "beta", 2)
+VERSION = (0, 9, 5, "final", 0)
 
 
 def get_semver_version(version):
     "Returns the semver version (X.Y.Z[-(alpha|beta)]) from VERSION"
     main = ".".join(str(x) for x in version[:3])
-
+    
     sub = ""
     if version[3] != "final":
         sub = "-{}.{}".format(*version[3:])
