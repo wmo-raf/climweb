7c9fab52f8987809db94fa536482651956b6a084
Erick Otenyo <otenyo.erick@gmail.com>
2025-04-02 16:37:17 +0300

    Automatically include css in js bundle

--- climweb/src/climweb/pages/home/home-map-vue/vite.config.js
+++ climweb/src/climweb/pages/home/home-map-vue/vite.config.js
@@ -4,12 +4,14 @@ import {resolve} from 'path'
 import {defineConfig} from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
+import cssInjectedByJsPlugin from "vite-plugin-css-injected-by-js";
 
 // https://vite.dev/config/
 export default defineConfig({
     plugins: [
         vue(),
         vueDevTools(),
+        cssInjectedByJsPlugin({jsAssetsFilterFunction: () => true}),
     ],
     resolve: {
         alias: {
@@ -26,5 +28,5 @@ export default defineConfig({
                 entryFileNames: '[name].js',
             },
         },
-    }
+    },
 })
