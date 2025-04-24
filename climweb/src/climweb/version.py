51c36380044a393deba0456a00a05465ed7367c8
Erick Otenyo <otenyo.erick@gmail.com>
2025-04-24 10:34:13 +0300

    Bump version

--- climweb/src/climweb/version.py
+++ climweb/src/climweb/version.py
@@ -1,6 +1,6 @@
 # major.minor.patch.release.number
 # release must be one of alpha, beta, rc, or final
-VERSION = (0, 9, 7, "final", 0)
+VERSION = (0, 9, 8, "beta", 1)
 
 
 def get_semver_version(version):
