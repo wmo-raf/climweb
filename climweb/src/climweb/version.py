aa7113f2b45f6b24253461c5c195a4aadecf3f45
Erick Otenyo <otenyo.erick@gmail.com>
2025-07-07 13:30:16 +0300

    Updates (#367)
    * Update vite version
    
    * Update dependencies
    
    * Update Geomanager to 0.5.6 fixing uuid filtering error

--- climweb/src/climweb/version.py
+++ climweb/src/climweb/version.py
@@ -1,6 +1,6 @@
 # major.minor.patch.release.number
 # release must be one of alpha, beta, rc, or final
-VERSION = (1, 0, 0, "final", 0)
+VERSION = (1, 0, 1, "final", 0)
 
 
 def get_semver_version(version):
