51a82f874a3673b5c064f419a227d16c9d2e4145
Grace Amondi <miswa.grace@gmail.com>
2025-09-04 15:56:05 +0300

    add banner color and image

--- climweb/src/climweb/pages/dashboards/models.py
+++ climweb/src/climweb/pages/dashboards/models.py
@@ -5,6 +5,7 @@ from wagtail.fields import RichTextField
 from django.db import models
 from wagtail_color_panel.edit_handlers import NativeColorPanel
 from climweb.pages.dashboards.blocks import DashboardSectionBlock
+from django.utils.translation import gettext_lazy as _
 
 class DashboardGalleryPage(Page):
     subpage_types = ['dashboards.DashboardPage']  # only allow DashboardPage children
@@ -23,11 +24,20 @@ class DashboardPage(Page):
     template = 'dashboards/dashboard_page.html'
     parent_page_types = ['dashboards.DashboardGalleryPage']
     banner_title = models.CharField(max_length=255)
-    banner_description = RichTextField(help_text="Banner description")
+    banner_description = RichTextField(help_text=_("Banner description"))
     banner_background_color = models.CharField(
         max_length=7,
         default="#f5f5f5",
-        help_text="Hex color code for banner background (e.g., #ffffff)"
+        help_text=_("Hex color code for banner background (e.g., #ffffff)")
+    )
+    banner_image = models.ForeignKey(
+        'wagtailimages.Image',
+        verbose_name=_("Banner Image"),
+        help_text=_("A high quality banner image"),
+        null=True,
+        blank=True,
+        on_delete=models.SET_NULL,
+        related_name='+',
     )
 
     body = StreamField([
@@ -39,6 +49,7 @@ class DashboardPage(Page):
             FieldPanel("banner_title"),
             FieldPanel("banner_description"),
             NativeColorPanel("banner_background_color"),
+            FieldPanel("banner_image"),
         ], heading="Banner Settings"),
         FieldPanel("body"),
     ]
