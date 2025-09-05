83bbf8d902a8025eb94c96b1cd09907b505f7f39
Grace Amondi <miswa.grace@gmail.com>
2025-09-05 14:41:35 +0300

    bump version

--- climweb/src/climweb/version.py
+++ climweb/src/climweb/version.py
@@ -1,6 +1,6 @@
 # major.minor.patch.release.number
 # release must be one of alpha, beta, rc, or final
-VERSION = (1, 0, 0, "final", 0)
+VERSION = (1, 0, 2, "beta", 2)
 
 
 def get_semver_version(version):
