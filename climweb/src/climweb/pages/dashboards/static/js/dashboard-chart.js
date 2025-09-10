5338615ca74847aba423bdc972f8607b47782f16
Grace Amondi <miswa.grace@gmail.com>
2025-09-10 13:27:59 +0300

    sort warming stripes data

--- climweb/src/climweb/pages/dashboards/static/js/dashboard-chart.js
+++ climweb/src/climweb/pages/dashboards/static/js/dashboard-chart.js
@@ -404,6 +404,7 @@ async function renderWarmingStripes(container) {
             container.innerHTML = `<p style="color:red;">No data</p>`;
             return;
         }
+        data.sort((a, b) => new Date(a.date) - new Date(b.date));
 
         // Prepare values and formatted labels
         const values = data.map(d => d.value);
