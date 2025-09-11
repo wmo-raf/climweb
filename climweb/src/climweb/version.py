c17fd5b23ef2f1eef9837fa4be5d0a471a490889
Grace Amondi <miswa.grace@gmail.com>
2025-09-12 01:41:50 +0300

    Dashboard fix date and time selectors (#375)
    * doc transl
    
    * add dashboard pages
    
    * add dashboard pages
    
    * chart color, text title image block
    
    * remove conflicts
    
    * dashboard map render layers
    
    * backend logic
    
    * add chart visualization
    
    * chart visualization
    
    * add date selector
    
    * add date selector
    
    * add info and redirect to mapviewer buttons
    
    * functional calender dates
    
    * add raster tile visualization
    
    * add raster tile visualization
    
    * mapviewier url button
    
    * save admin_area_geostore path
    
    * add warming stripes
    
    * render warming stripes data
    
    * fix daterange input
    
    * add paramselectorconfigs
    
    * metadata modal
    
    * parameter selector for raster tile maps
    
    * add scatter plot, area chart and bar graph
    
    * refactor styling
    
    * add banner color and image
    
    * initial atlas docs
    
    * reorder atlas menu items
    
    * bump version
    
    * finalize atlas docs
    
    * code refactor
    
    * sort warming stripes data
    
    * dashboard refactor
    warming stripes sort order correctly
    map calendar linked to data date format
    human friendly dates
    
    * dashboard refactor
    warming stripes sort order correctly
    map calendar linked to data date format
    human friendly dates
    
    * bump version
    
    * undo bump version
    
    * undo bump version
    
    * Fix DataRequestPage to include recaptcha field and update to capcomposer from alertwise (#373)
    
    * Update DataRequestPage form_builder to include extend WagtailCaptchaFormBuilder for recaptcha
    
    * Bump version
    
    * Update alertwise to capcomposer
    
    * Dashboard Refactor (#374)
    
    * doc transl
    
    * add dashboard pages
    
    * add dashboard pages
    
    * chart color, text title image block
    
    * remove conflicts
    
    * dashboard map render layers
    
    * backend logic
    
    * add chart visualization
    
    * chart visualization
    
    * add date selector
    
    * add date selector
    
    * add info and redirect to mapviewer buttons
    
    * functional calender dates
    
    * add raster tile visualization
    
    * add raster tile visualization
    
    * mapviewier url button
    
    * save admin_area_geostore path
    
    * add warming stripes
    
    * render warming stripes data
    
    * fix daterange input
    
    * add paramselectorconfigs
    
    * metadata modal
    
    * parameter selector for raster tile maps
    
    * add scatter plot, area chart and bar graph
    
    * refactor styling
    
    * add banner color and image
    
    * initial atlas docs
    
    * reorder atlas menu items
    
    * bump version
    
    * finalize atlas docs
    
    * code refactor
    
    * sort warming stripes data
    
    * dashboard refactor
    warming stripes sort order correctly
    map calendar linked to data date format
    human friendly dates
    
    * dashboard refactor
    warming stripes sort order correctly
    map calendar linked to data date format
    human friendly dates
    
    * bump version
    
    * undo bump version
    
    * undo bump version
    
    ---------
    
    Co-authored-by: Grace Amondi <miswa.grace@gmail.com>
    
    * change calendar and time selectors for charts and maps
    add seo for dashboards
    
    * remove flatpickr lin
    
    * remove flatpickr lin
    
    ---------
    
    Co-authored-by: Erick Otenyo <otenyo.erick@gmail.com>

--- climweb/src/climweb/version.py
+++ climweb/src/climweb/version.py
@@ -1,6 +1,6 @@
 # major.minor.patch.release.number
 # release must be one of alpha, beta, rc, or final
-VERSION = (1, 0, 3, "final", 0)
+VERSION = (1, 0, 4, "beta", 1)
 
 
 def get_semver_version(version):
