0650c655d2232385b4398def70d3cb8f7baff057
Erick Otenyo <otenyo.erick@gmail.com>
2026-02-11 17:32:19 +0300

    Upgrade Geomanager (#439)
    * Upgrade geomanager to allow django-large-iamge package installation without setuptools
    
    * Bump version
    
    ---------
    
    Co-authored-by: Grace Amondi <miswa.grace@gmail.com>

--- climweb/src/climweb/version.py
+++ climweb/src/climweb/version.py
@@ -2,6 +2,7 @@
 # release must be one of alpha, beta, rc, or final
 VERSION = (1, 0, 8, "beta", 2)
 
+
 def get_semver_version(version):
     "Returns the semver version (X.Y.Z[-(alpha|beta)]) from VERSION"
     main = ".".join(str(x) for x in version[:3])
