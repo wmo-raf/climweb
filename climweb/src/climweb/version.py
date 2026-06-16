fc4b29626f76189fd609a2a7781bf2f09ceb6380
Grace Amondi <miswa.grace@gmail.com>
2026-06-16 17:54:52 +0300

    bump forecastmanager, wagtailsurveyform (#658)

--- climweb/src/climweb/version.py
+++ climweb/src/climweb/version.py
@@ -1,6 +1,6 @@
 # major.minor.patch.release.number
 # release must be one of alpha, beta, rc, or final
-VERSION = (1, 1, 7, "final", 0)
+VERSION = (1, 1, 8, "beta", 1)
 
 
 def get_semver_version(version):
